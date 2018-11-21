ea9ca0e8968f61be77088e9912e739929af30458
kobi <kobi.b@inner-active.com>
2018-11-21 19:11:28 +0200

    wip

---
+++ test/intBasedEnum.test.ts
@@ -0,0 +1,10 @@
+import {intBasedEnum} from "../src/intBasedEnum"
+import {expect} from "chai"
+
+describe("intBasedEnum.test", function () {
+  it("should", () => {
+    expect(
+      intBasedEnum(["boolean", "number", "string"]).comment
+    ).eq("1 - boolean, 2 - number, 3 - string")
+  })
+})
